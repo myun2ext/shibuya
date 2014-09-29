95634161692be7ca92642d55963b2c0b6cfad9b7
myun2 <myun2@nwhite.info>
2014-09-30 02:26:00 +0900

    foreing key index

--- db/schema.sql
+++ db/schema.sql
@@ -18,12 +18,14 @@ CREATE UNIQUE INDEX user_name_idx ON users (name);
 -- nodes table
 CREATE TABLE nodes (
   id BIGINT NOT NULL AUTO_INCREMENT,
+  parent_node_id BIGINT,
   name VARCHAR(64) NOT NULL,
   screen_name VARCHAR(128) NOT NULL,
   created_at datetime NOT NULL,
   PRIMARY KEY (id)
 );
 CREATE UNIQUE INDEX node_name_idx ON nodes (name);
+ALTER TABLE nodes ADD FOREIGN KEY(parent_node_id) REFERENCES nodes(id);
 
 -- chat_logs table
 CREATE TABLE chat_logs (
@@ -32,3 +34,4 @@ CREATE TABLE chat_logs (
   created_at datetime NOT NULL,
   PRIMARY KEY (id)
 );
+ALTER TABLE chat_logs ADD FOREIGN KEY(node_id) REFERENCES nodes(id);
