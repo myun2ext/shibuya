ea1c876a032604d5abd0f65e5a8470ed54379e3f
myun2 <myun2@nwhite.info>
2014-09-30 02:48:06 +0900

    node seed, favorites table

--- db/schema.sql
+++ db/schema.sql
@@ -20,12 +20,22 @@ CREATE TABLE nodes (
   id BIGINT NOT NULL AUTO_INCREMENT,
   parent_node_id BIGINT,
   name VARCHAR(64) NOT NULL,
-  screen_name VARCHAR(128) NOT NULL,
+  screen_name VARCHAR(256) NOT NULL,
   created_at datetime NOT NULL,
   PRIMARY KEY (id)
 );
-CREATE UNIQUE INDEX node_name_idx ON nodes (name);
+CREATE UNIQUE INDEX node_name_idx ON nodes (parent_node_id, name);
 ALTER TABLE nodes ADD FOREIGN KEY(parent_node_id) REFERENCES nodes(id);
+
+-- favorites table
+CREATE TABLE favorites (
+  user_id BIGINT NOT NULL,
+  node_id BIGINT NOT NULL,
+  created_at datetime NOT NULL,
+  PRIMARY KEY (user_id, node_id)
+);
+ALTER TABLE favorites ADD FOREIGN KEY(user_id) REFERENCES users(id);
+ALTER TABLE favorites ADD FOREIGN KEY(node_id) REFERENCES nodes(id);
 
 -- chat_logs table
 CREATE TABLE chat_logs (
--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,17 @@
 use shibuya_development;
+
+INSERT INTO nodes (name,  screen_name, created_at, parent_node_id) VALUES
+  ("Shibuya",   "渋谷", NOW(), NULL),
+  ("Tokyo",     "東京", NOW(), NULL),
+  ("Shinagawa", "品川", NOW(), NULL),
+
+  ("Sakuragaoka", "桜ヶ丘", NOW(), 1),
+  ("Dogenzaka",   "道玄坂", NOW(), 1),
+  ("Nanpeidai",   "南平台", NOW(), 1),
+
+  ("Shibuya109",  "渋谷109", NOW(), 5)
+;
+
 INSERT INTO users (name, screen_name, encrypted_password, password_salt, created_at) VALUES
   ("test1", "Test User", "0000", "000000", NOW())
 ;
