732738d112bda489bc6cd4dd0bb785faf821e844
myun2 <myun2@nwhite.info>
2014-09-29 15:49:18 +0000

    unicorn start, stop, restart

--- unicorn.sh
+++ unicorn.sh
@@ -1,1 +1,11 @@
-unicorn -c unicorn.rb -E production
+#/bin/sh
+
+if [ "$1" = "start" ]; then
+  unicorn -c unicorn.rb -E production -D
+
+elif [ "$1" = "restart" ]; then
+  kill -HUP `cat /tmp/myun2.shibuya.unicorn.pid`
+
+elif [ "$1" = "restart" ]; then
+  kill -HUP `cat /tmp/myun2.shibuya.unicorn.pid`
+fi
