ed4dde967edbeb6fb836e22e83ac3ef2222a9847
myun2 <myun2@nwhite.info>
2014-09-30 16:04:56 +0900

    app routes

--- app.rb
+++ app.rb
@@ -22,6 +22,23 @@ module Shibuya
       def db_connection
         Thread.current[:db_connection] ||= Shibuya::Db.new(db_config)
       end
+
+      def fetch_node_path
+        path = params[:splat].first.split("/")
+        @node_path = []
+        path.each do |name|
+          if @node_path.length == 0
+            node = Node.find_by_name(name, db_connection)
+          else
+            node = Node.find_by_name_of_children(@node_path.last, name, db_connection)
+          end
+          if node == nil
+            return erb "404"
+          end
+          @node_path << node
+        end
+        @nodes = Node.children(@node_path.last.id, db_connection)
+      end
     end
 
     get '/' do
@@ -33,22 +50,30 @@ module Shibuya
       erb :login
     end
 
-    get '/nodes/*' do
-      path = params[:splat].first.split("/")
-      @node_path = []
-      path.each do |name|
-        if @node_path.length == 0
-          node = Node.find_by_name(name, db_connection)
-        else
-          node = Node.find_by_name_of_children(@node_path.last, name, db_connection)
-        end
-        if node == nil
-          return erb "404"
-        end
-        @node_path << node
-      end
-      @nodes = Node.children(@node_path.last.id, db_connection)
+    get '/chat_logs/*' do
+      fetch_node_path
+      erb :chat_log
+    end
 
+    post '/chat_logs/*' do
+      fetch_node_path
+      post_chat
+      redirect to('/chat_logs/' + params[:splat])
+    end
+
+    get '/nodes/*/new' do
+      fetch_node_path
+      erb :new_node
+    end
+
+    post'/nodes/*' do
+      fetch_node_path
+      post_node
+       redirect to('/nodes/' + params[:splat])
+    end
+
+    get '/nodes/*' do
+      fetch_node_path
       erb :index
     end
   end
