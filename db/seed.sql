840b6f95cf7c5b63b29a06f45bcc10990c2822be
myun2 <myun2@nwhite.info>
2014-09-30 23:59:39 +0900

    color

--- db/seed.sql
+++ db/seed.sql
@@ -7,9 +7,10 @@ INSERT INTO nodes (name, screen_name, color, created_at, parent_node_id) VALUES
   ("Tokyo",     "東京", "37B7DD", NOW(), 1),
   ("Shinagawa", "品川", "3689DB", NOW(), 1),
 
-  ("Sakuragaoka", "桜ヶ丘", "FFAAEC", NOW(), 2),
-  ("Dogenzaka",   "道玄坂", "AA693D", NOW(), 2),
-  ("Nanpeidai",   "南平台", "82A4D6", NOW(), 2),
+-- EA628F
+  ("Sakuragaoka", "桜ヶ丘", "EA4DD3", NOW(), 2),
+  ("Dogenzaka",   "道玄坂", "EA9662", NOW(), 2),
+  ("Nanpeidai",   "南平台", "6774EA", NOW(), 2),
 
   ("Shibuya109",  "渋谷109", "EA59BA", NOW(), 6)
 ;
