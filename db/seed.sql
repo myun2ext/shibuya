4f4c45d2a9aa0ed35d36948e7179d08f81461f7b
myun2 <myun2@nwhite.info>
2014-09-30 13:49:41 +0900

    fix seed

--- db/seed.sql
+++ db/seed.sql
@@ -3,27 +3,15 @@ use shibuya_development;
 INSERT INTO nodes (name, screen_name, color, created_at, parent_node_id) VALUES
   ("root",       "", "fff", NOW(), NULL),
 
-  ("Geography",  "地理", "fff", NOW(), NULL),
-  ("3",  "地理", "fff", NOW(), NULL),
-  ("4",  "地理", "fff", NOW(), NULL),
-  ("5",  "地理", "fff", NOW(), NULL),
-  ("5",  "地理", "fff", NOW(), NULL),
-  ("6",  "地理", "fff", NOW(), NULL),
-  ("7",  "地理", "fff", NOW(), NULL),
-  ("8",  "地理", "fff", NOW(), NULL),
+  ("Shibuya",   "渋谷", "278C3E", NOW(), 1),
+  ("Tokyo",     "東京", "37B7DD", NOW(), 1),
+  ("Shinagawa", "品川", "3689DB", NOW(), 1),
 
-  ("Country",    "国",   "fff", NOW(), 8),
-  ("Japan",      "日本", "fff", NOW(), 9),
+  ("Sakuragaoka", "桜ヶ丘", "FFAAEC", NOW(), 2),
+  ("Dogenzaka",   "道玄坂", "AA693D", NOW(), 2),
+  ("Nanpeidai",   "南平台", "82A4D6", NOW(), 2),
 
-  ("Shibuya",   "渋谷", "278C3E", NOW(), 10),
-  ("Tokyo",     "東京", "37B7DD", NOW(), 10),
-  ("Shinagawa", "品川", "3689DB", NOW(), 10),
-
-  ("Sakuragaoka", "桜ヶ丘", "FFAAEC", NOW(), 11),
-  ("Dogenzaka",   "道玄坂", "AA693D", NOW(), 11),
-  ("Nanpeidai",   "南平台", "82A4D6", NOW(), 11),
-
-  ("Shibuya109",  "渋谷109", "EA59BA", NOW(), 15)
+  ("Shibuya109",  "渋谷109", "EA59BA", NOW(), 6)
 ;
 
 INSERT INTO users (name, screen_name, encrypted_password, password_salt, created_at) VALUES
