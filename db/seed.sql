6ef89da3996291cd95215d73063b1c86f4c44427
myun2 <myun2@nwhite.info>
2014-10-01 00:02:30 +0900

    color2

--- db/seed.sql
+++ db/seed.sql
@@ -1,16 +1,16 @@
 use shibuya_development;
 
 INSERT INTO nodes (name, screen_name, color, created_at, parent_node_id) VALUES
-  ("root",       "", "fff", NOW(), NULL),
+  ("Tokyo",     "東京", "fff", NOW(), NULL),
 
   ("Shibuya",   "渋谷", "278C3E", NOW(), 1),
   ("Tokyo",     "東京", "37B7DD", NOW(), 1),
   ("Shinagawa", "品川", "3689DB", NOW(), 1),
 
 -- EA628F
-  ("Sakuragaoka", "桜ヶ丘", "EA4DD3", NOW(), 2),
-  ("Dogenzaka",   "道玄坂", "EA9662", NOW(), 2),
-  ("Nanpeidai",   "南平台", "6774EA", NOW(), 2),
+  ("Sakuragaoka", "桜ヶ丘", "E35AED", NOW(), 2),
+  ("Dogenzaka",   "道玄坂", "7F1033", NOW(), 2),
+  ("Nanpeidai",   "南平台", "2A32C1", NOW(), 2),
 
   ("Shibuya109",  "渋谷109", "EA59BA", NOW(), 6)
 ;
