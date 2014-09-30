9256146ae3349bcd2f5456a7122147bdb501db72
myun2 <myun2@nwhite.info>
2014-09-30 14:02:28 +0900

    viewing from DB!

--- app/shibuya/models/node.rb
+++ app/shibuya/models/node.rb
@@ -3,12 +3,16 @@ module Shibuya
     attr_reader :id, :parent_node_id, :name, :color, :screen_name, :created_at
 
     def initialize(params)
-      @id = params[:id]
-      @parent_node_id = params[:parent_node_id]
-      @name = params[:name]
-      @color = params[:color]
-      @screen_name = params[:screen_name]
-      @created_at = params[:created_at]
+      @id = params["id"]
+      @parent_node_id = params["parent_node_id"]
+      @name = params["name"]
+      @color = params["color"]
+      @screen_name = params["screen_name"]
+      @created_at = params["created_at"]
+    end
+
+    def path
+      name
     end
 
     def self.children(id, db_connection)
