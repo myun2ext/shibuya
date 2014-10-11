144915f00724b4dec74961070921f4cfab0de411
myun2 <myun2@nwhite.info>
2014-10-11 16:22:09 +0900

    app

--- app.rb
+++ app.rb
@@ -1,11 +1,21 @@
 require './application_base'
-
 require './app/shibuya/models/node'
 require './app/shibuya/models/chat_log'
 require './app/shibuya/controllers/node'
 require './app/shibuya/controllers/chat_log'
+require './app/shibuya/controllers/authentication'
+require './app/shibuya/db_config'
+require './app/shibuya/modules'
 
 module Shibuya
   class App < Application::Base
+    include AssetsRouter
+
+    configure do
+      ShibuyaRecord.configure { |conf| conf.db_connection_settings = Shibuya::DbConfig.db_config }
+    end
+
+    routing do
+    end
   end
 end
--- application_base.rb
+++ application_base.rb
@@ -5,21 +5,14 @@ $:.unshift File.dirname(__FILE__)
 $:.unshift File.expand_path('./lib/shibuya_record/', File.dirname(__FILE__))
 require 'shibuya_record'
 
-require './app/shibuya/controllers/authentication'
-require './app/shibuya/db_config'
-require './app/shibuya/modules'
-
 module Shibuya
   module Application
     class Base < Sinatra::Base
-      include AssetsRouter
-
       configure do
         enable :logging
         file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
         file.sync = true
         use Rack::CommonLogger, file
-        ShibuyaRecord.configure { |conf| conf.db_connection_settings = Shibuya::DbConfig.db_config }
       end
     end
   end
